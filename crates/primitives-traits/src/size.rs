e6fa203585d1d747c1a9714205e8457f394fcbf6
Federico Gimenez <federico.gimenez@gmail.com>
2025-03-11 16:29:31 +0100

    clippy

--- crates/primitives-traits/src/size.rs
+++ crates/primitives-traits/src/size.rs
@@ -114,7 +114,7 @@ impl<T: InMemorySize> InMemorySize for Vec<T> {
 
 impl InMemorySize for u64 {
     fn size(&self) -> usize {
-        return core::mem::size_of::<Self>();
+        core::mem::size_of::<Self>()
     }
 }
 
